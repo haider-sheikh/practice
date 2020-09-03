330d8ee2d03c2acaa3b88374855ab03248e99e93
Haider Shahid <haider.shahid@7vals.com>
2020-09-03 18:59:44 +0500

    Haider Commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_083934) do
+ActiveRecord::Schema.define(version: 2020_08_31_132318) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "body"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_083934) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin?", default: false
   end
 
   add_foreign_key "comments", "users"
